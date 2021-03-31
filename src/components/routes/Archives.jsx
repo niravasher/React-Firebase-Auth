8a413d6e42243a6da4d077bf4cd6df977984e529
nakulgupta112233 <nakulgupta112233@gmail.com>
2021-03-31 21:11:58 +0530

    new3

--- src/components/routes/Archives.jsx
+++ src/components/routes/Archives.jsx
@@ -2,61 +2,56 @@ import React, { Component } from "react";
 import Navbar from "./Navbar";
 import Footer from "../routes/footer";
 import '../css/style.css'
-
 class Archives extends Component {
   render() {
     return (
       <>
-      <div className="bg-new3">
-        <div className="row">
-          <div className="col-lg-12 col-md-12 col-sm-12 text-center">
-            <Navbar />
+        <div className="bg-new3">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 text-center">
+              <Navbar />
+            </div>
           </div>
-        </div>
-        <div className="container-fluid">
-          <div className="container-fluid ">
+          <div className="container-fluid">
             <div className="row m-4">
               <div className="col-lg-1"></div>
-              <div className="col-lg-10 col-sm-12 upper_left text-white mr-1 ">
-                <div className="table-responsive-md">
-                  <table className="table table-bordered table-hover bg-light">
-                    <thead>
-                      <tr>
-                        <th colSpan="4" className="text-center">Company</th>
-                      </tr>
-                    </thead>
-                    <tbody className="bg-light">
-                      <tr>
-                        <td className="td">1</td>
-                        <td className="thfirst"><span>Mark</span></td>
-                        <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classNameical Latin literature from 45 BC</td>
-                        <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classNameical Latin literature from 45 BC</td>
-                      </tr>
-                      <tr>
-                        <td className="td"><span>2</span></td>
-                        <td className="thfirst"><span>Some random title</span></td>
-                        <td>making it over 2000 years old. Richard McClintock, a Latin<span className="bg-primary phalf">Some random title</span></td>
-                        <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classNameical Latin literature from 45 BC</td>
-                      </tr>
-                      <tr>
-                        <td className="td"><span>3</span></td>
-                        <td className="thfirst"><span>Mark</span></td>
-                        <td><span className="bg-success phalf">Mark</span>passage, and going through the cites of the word in classNameical</td>
-                        <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classNameical Latin literature from 45 BC</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+              <div className="col-lg-12 col-sm-12 text-white upper_left mr-1">
+                <table class="table table-bordered table-hover bg-light table-responsive">
+                  <thead>
+                    <tr>
+                      <th colspan="4" className="text-center">Company</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="td">1</td>
+                      <td className="thfirst"><span>Mark</span></td>
+                      <td><span className="bg-primary phalf">Change</span></td>
+                      <td><span>Mark</span></td>
+                    </tr>
+                    <tr>
+                      <td className="td"><span>2</span></td>
+                      <td className="thfirst"><span>Some random title</span></td>
+                      <td>when an unknown printer took a galley of type and scrambled it to <span className="bg-info phalf">Some random title</span>make a type specimen book. It has survived not only five centuries</td>
+                      <td><span>Mark</span></td>
+                    </tr>
+                    <tr>
+                      <td className="td"><span>101</span></td>
+                      <td className="thfirst"><span>this</span></td>
+                      <td>It was popularised in the 1960s with <span className="bg-warning phalf">abcde sdfieh</span> the release of Letraset sheets containing Lorem<span className="bg-dark phalf">Mark</span>Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including</td>
+                      <td>It was popularised in the 1960s with <span className="bg-warning phalf">abcde sdfieh</span> the release of Letraset sheets containing Lorem<span className="bg-dark phalf">Mark</span>Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
             <div className="row m-4">
-
               <div className="col-lg-1"></div>
-              <div className="col-lg-5 col-sm-12 text-white upper_left ">
-                <table className="table table-bordered table-hover bg-light table-responsive-md">
+              <div className="col-lg-5 col-sm-12 text-white upper_left mr-1">
+                <table class="table table-bordered table-hover bg-light">
                   <thead>
                     <tr>
-                      <th colSpan="3" className="text-center">Young Male</th>
+                      <th colspan="3" className="text-center">Young Male</th>
                     </tr>
                   </thead>
                   <tbody>
@@ -71,21 +66,21 @@ class Archives extends Component {
                       <td>when an unknown printer took a galley of type and scrambled it to <span className="bg-info phalf">Some random title</span>make a type specimen book. It has survived not only five centuries</td>
                     </tr>
                     <tr>
-                      <td className="td"><span>1</span></td>
+                      <td className="td"><span>101</span></td>
                       <td className="thfirst"><span>this</span></td>
-                      <td>It was popularised in the 1960s with <span className="bg-warning phalf">abcde sdfieh</span>containing Lorem<span className="bg-dark phalf">Mark</span>Ipsumecently with desktop publishing software like Aldus PageMaker including</td>
+                      <td>It was popularised in the 1960s with <span className="bg-warning phalf">abcde sdfieh</span> the release of Letraset sheets containing Lorem<span className="bg-dark phalf">Mark</span>Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
               <div className="col-lg-5 col-sm-12 upper_left">
-                <table className="table table-bordered table-hover bg-light table-responsive-md">
+                <table class="table table-bordered table-hover bg-light">
                   <thead>
                     <tr>
-                      <th colSpan="3" className="text-center">Young Female</th>
+                      <th colspan="3" className="text-center">Young Female</th>
                     </tr>
                   </thead>
-                  <tbody className="bg-light">
+                  <tbody class="bg-light">
                     <tr>
                       <td className="td">1</td>
                       <td className="thfirst"><span>to something</span></td>
@@ -99,27 +94,26 @@ class Archives extends Component {
                     <tr>
                       <td className="td">3</td>
                       <td className="thfirst"><span>I sall </span></td>
-                      <td><span className="bg-warning phalf">Kill you </span>make a type specimen book. It has survived not only five centuries </td>
+                      <td><span className="bg-warning phalf">Kill you</span></td>
                     </tr>
                   </tbody>
                 </table>
               </div>
             </div>
-
             <div className="row m-4">
               <div className="col-lg-1"></div>
               <div className="col-lg-5 col-sm-12 upper_left text-white mr-1">
-                <table className="table table-bordered table-hover bg-light table-responsive-md">
+                <table class="table table-bordered table-hover bg-light">
                   <thead>
                     <tr>
-                      <th colSpan="3" className="text-center">Old Male</th>
+                      <th colspan="3" className="text-center">Old Male</th>
                     </tr>
                   </thead>
-                  <tbody className="bg-light">
+                  <tbody class="bg-light">
                     <tr>
                       <td className="td">1</td>
                       <td className="thfirst"><span>Mark</span></td>
-                      <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classNameical Latin literature from 45 BC</td>
+                      <td>Contrary to popular belief, Lorem Ipsum is not simply random text<span className="bg-danger phalf">Mark</span>It has roots in a piece of classical Latin literature from 45 BC</td>
                     </tr>
                     <tr>
                       <td className="td"><span>2</span></td>
@@ -129,19 +123,19 @@ class Archives extends Component {
                     <tr>
                       <td className="td"><span>3</span></td>
                       <td className="thfirst"><span>Mark</span></td>
-                      <td><span className="bg-success phalf">Mark</span>passage, and going through the cites of the word in classNameical</td>
+                      <td><span className="bg-success phalf">Mark</span>passage, and going through the cites of the word in classical</td>
                     </tr>
                   </tbody>
                 </table>
               </div>
               <div className="col-lg-5 col-sm-12 upper_left">
-                <table className="table table-bordered table-hover bg-light table-responsive-md">
+                <table class="table table-bordered table-hover bg-light">
                   <thead>
                     <tr>
-                      <th colSpan="3" className="text-center">Old Female</th>
+                      <th colspan="3" className="text-center">Old Female</th>
                     </tr>
                   </thead>
-                  <tbody className="bg-light">
+                  <tbody class="bg-light">
                     <tr>
                       <td className="td">1</td>
                       <td className="thfirst"><span>Mark</span></td>
@@ -163,12 +157,11 @@ class Archives extends Component {
             </div>
           </div>
         </div>
-      </div>
-      <div className="row">
+        <div className="row">
           <Footer />
-      </div>
-    </>
-  );
+        </div>
+      </>
+    );
   };
 }
 export default Archives;
