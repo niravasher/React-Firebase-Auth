35accc322f99543c0c20f2d8075ebd41139af558
niravasher <f20171066@pilani.bits-pilani.ac.in>
2021-04-04 15:13:13 +0530

    added card to demographhics

--- src/components/dashboard/Demographics.jsx
+++ src/components/dashboard/Demographics.jsx
@@ -31,106 +31,104 @@ class Demographics extends Component {
       <>
         <div className="container fontchange">
           <div className="container">
-            <div className="row">
-              <div className="col text-center">
-                <h3>
-                  <label className="unselectable-text">Statistics</label>
-                </h3>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-12 text-center">
+            <div className="card shadow mt-2">
+              <div className="card-header"><h4 className="text-center">Statistics</h4></div>
+              <div className="card-body">
                 <div className="row">
-                  <div className="col">
-                    <h6>
-                      <label className="unselectable-text">Old Male</label>
-                    </h6>
-                  </div>
-                  <div className="col">
-                    <h6>
-                      <label className="unselectable-text">Old Female</label>
-                    </h6>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col">
-                    <div className="demographics1">
-                      <img
-                        className="checkmark"
-                        src={Oldmale}
-                        alt=""
-                      />
-                      <input
-                        id="checkbox1"
-                        type="checkbox"
-                        className="checkbox"
-                        onChange={this.props.clickIcon}
-                        checked={this.props.first}
-                      />
+                  <div className="col-lg-12 text-center">
+                    <div className="row">
+                      <div className="col">
+                        <h6>
+                          <label className="unselectable-text">Old Male</label>
+                        </h6>
+                      </div>
+                      <div className="col">
+                        <h6>
+                          <label className="unselectable-text">Old Female</label>
+                        </h6>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col">
+                        <div className="demographics1">
+                          <img
+                            className="checkmark"
+                            src={Oldmale}
+                            alt=""
+                          />
+                          <input
+                            id="checkbox1"
+                            type="checkbox"
+                            className="checkbox"
+                            onChange={this.props.clickIcon}
+                            checked={this.props.first}
+                          />
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="demographics1">
+                          <img
+                            className="checkmark"
+                            src={Oldfemale}
+                            alt=""
+                          />
+                          <input
+                            id="checkbox2"
+                            type="checkbox"
+                            className="checkbox"
+                            onChange={this.props.clickIcon}
+                            checked={this.props.second}
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
-                  <div className="col">
-                    <div className="demographics1">
-                      <img
-                        className="checkmark"
-                        src={Oldfemale}
-                        alt=""
-                      />
-                      <input
-                        id="checkbox2"
-                        type="checkbox"
-                        className="checkbox"
-                        onChange={this.props.clickIcon}
-                        checked={this.props.second}
-                      />
+                  <div className="col-lg-12 text-center">
+                    <div className="row">
+                      <div className="col">
+                        <h6>
+                          <label className="unselectable-text">Young Male</label>
+                        </h6>
+                      </div>
+                      <div className="col">
+                        <h6>
+                          <label className="unselectable-text">Young Female</label>
+                        </h6>
+                      </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-lg-12 text-center">
-                <div className="row">
-                  <div className="col">
-                    <h6>
-                      <label className="unselectable-text">Young Male</label>
-                    </h6>
-                  </div>
-                  <div className="col">
-                    <h6>
-                      <label className="unselectable-text">Young Female</label>
-                    </h6>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col">
-                    <div className="demographics1">
-                      <img
-                        className="checkmark"
-                        src="https://img.icons8.com/offices/80/000000/person-male-skin-type-4.png"
-                        alt=""
-                      />
-                      <input
-                        id="checkbox3"
-                        type="checkbox"
-                        className="checkbox"
-                        onChange={this.props.clickIcon}
-                        checked={this.props.third}
-                      />
-                    </div>
-                  </div>
-                  <div className="col">
-                    <div className="demographics1 ">
-                      <img
-                        className="checkmark"
-                        src="https://img.icons8.com/officel/80/000000/person-female.png"
-                        alt=""
-                      />
-                      <input
-                        id="checkbox4"
-                        type="checkbox"
-                        className="checkbox"
-                        onChange={this.props.clickIcon}
-                        checked={this.props.fourth}
-                      />
+                    <div className="row">
+                      <div className="col">
+                        <div className="demographics1">
+                          <img
+                            className="checkmark"
+                            src="https://img.icons8.com/offices/80/000000/person-male-skin-type-4.png"
+                            alt=""
+                          />
+                          <input
+                            id="checkbox3"
+                            type="checkbox"
+                            className="checkbox"
+                            onChange={this.props.clickIcon}
+                            checked={this.props.third}
+                          />
+                        </div>
+                      </div>
+                      <div className="col">
+                        <div className="demographics1 ">
+                          <img
+                            className="checkmark"
+                            src="https://img.icons8.com/officel/80/000000/person-female.png"
+                            alt=""
+                          />
+                          <input
+                            id="checkbox4"
+                            type="checkbox"
+                            className="checkbox"
+                            onChange={this.props.clickIcon}
+                            checked={this.props.fourth}
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
