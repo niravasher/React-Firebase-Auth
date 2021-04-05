bef41aa9a39515a30de54829d98c874797823bdb
niravasher <f20171066@pilani.bits-pilani.ac.in>
2021-04-05 13:30:50 +0530

    added csv button

--- src/components/routes/Profile.jsx
+++ src/components/routes/Profile.jsx
@@ -5,7 +5,8 @@ import "../css/style.css";
 import bp from "../images/bp_new.svg";
 import axios from "axios";
 import ReactTooltip from 'react-tooltip';
-import { AiFillInfoCircle } from "react-icons/ai";
+import { AiFillInfoCircle, AiOutlineFileExcel } from "react-icons/ai";
+import { CSVLink } from "react-csv";
 
 class Profile extends Component {
   state = {
@@ -25,15 +26,31 @@ class Profile extends Component {
   }
 
   render() {
+    var headers = [
+      { label: "Good Data", key: "gooddata" },
+      { label: "Warning Data", key: "warningdata" },
+      { label: "Bad Data", key: "baddata" }
+    ];
+
+    var csv_data = [];
+    var good_csv_data = [];
+    var warning_csv_data = [];
+    var bad_csv_data = [];
+
     const { jsondata } = this.state;
     const goodData = jsondata.length ? (
       jsondata.map((data) => {
         if (data.status === "good") {
+          good_csv_data.push(data.emp_id)
           return (
             <tr key={data.id}>
               <td className="text-center text-font">{data.emp_id}</td>
             </tr>
           );
+        } else if (data.status === 'warning') {
+          warning_csv_data.push(data.emp_id)
+        } else if (data.status === 'bad') {
+          bad_csv_data.push(data.emp_id);
         }
       })
     ) : (
@@ -67,6 +84,12 @@ class Profile extends Component {
     ) : (
       <div className="center">No data here</div>
     );
+    
+    for (var i = 0 ; i < 10000 ; i++) {
+      csv_data.push({
+        gooddata: good_csv_data[i], warningdata: warning_csv_data[i], baddata: bad_csv_data[i]
+      })
+    }
 
     return (
       <div className="bg-new3">
@@ -287,6 +310,17 @@ class Profile extends Component {
           </div>
         </div>
         <br />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4"></div>
+            <div className="col-lg-4"></div>
+            <div className="col-lg-4 text-right">
+              <CSVLink data={csv_data} className="btn btn-primary mb-4" headers={headers} filename={"employee_data.csv"}>
+                Download CSV <span><AiOutlineFileExcel size="2em" className="mb-1" /></span>
+              </CSVLink>
+            </div>
+          </div>
+        </div>
         <div className="d-flex flex-wrap justify-content-around mb-3 text-center align-self-start">
           <div className="p-2 m-2 bg-success-imp rounded">
             <h3 className="text-center text-white pb-1">GOOD <AiFillInfoCircle data-tip="hello world" class="mb-1" /></h3>
