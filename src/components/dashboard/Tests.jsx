c434de4b2d44a96a0ad12c8ee85691f60ed4f12b
karanrajbhasin <82761215+karanrajbhasin@users.noreply.github.com>
2021-05-15 14:08:02 +0530

    Update Tests.jsx

--- src/components/dashboard/Tests.jsx
+++ src/components/dashboard/Tests.jsx
@@ -285,57 +285,57 @@ class Tests extends Component {
                 <h5>{this.state.nodeInnerText}</h5>
                 {this.state.nodeInnerText === "Complete Blood Count" && (
                   <p class="card-text">
-                    The complete blood count (CBC) evaluates the cells that circulate in blood, including:- 
-                     • Red blood cells (RBCs), 
-                     • White blood cells (WBCs), and 
-                     • Platelets (PLTs).
+                    The complete blood count (CBC) evaluates the cells that circulate in blood, including:-<br>
+                     • Red blood cells (RBCs), <br>
+                     • White blood cells (WBCs), and <br>
+                     • Platelets (PLTs).<br>
 
                   </p>
                 )}
                 {this.state.nodeInnerText === "Kidney" && (
-                  <p class="card-text">Kidney tests measure levels of various substances in the blood to determine the current health of your kidneys, including several 
-                   • minerals, 
-                   • electrolytes, 
-                   • proteins, and 
-                   • glucose (sugar).
+                  <p class="card-text">Kidney tests measure levels of various substances in the blood to determine the current health of your kidneys, including:-<br> 
+                   • minerals, <br>
+                   • electrolytes, <br>
+                   • proteins, and <br>
+                   • glucose (sugar).<br>
                   </p>
                 )}
                 {this.state.nodeInnerText === "Lipids" && (
-                  <p class="card-text">Lipid tests are a combination of test primarily to check for coronary heart disease, they measure:-  
-                   • high-density lipoproteins (HDL), 
-                   • low-density lipoproteins (LDL), 
-                   • very low-density lipoproteins (VLDL) and 
-                   • Triglycerides.
+                  <p class="card-text">Lipid tests are a combination of test primarily to check for coronary heart disease, they measure:- <br> 
+                   • high-density lipoproteins (HDL), <br>
+                   • low-density lipoproteins (LDL), <br>
+                   • very low-density lipoproteins (VLDL) and<br> 
+                   • Triglycerides.<br>
                   </p>
                 )}
                 {this.state.nodeInnerText === "Thyroid" && (
-                  <p class="card-text">Thyroid test is conducted to measure the function of the thyroid gland via a blood sample. The test measures values of:- 
-                   • Thyroxine (T4), 
-                   • Triiodothyronine (T3) and 
-                   • Thyroid stimulating hormone (TSH).
+                  <p class="card-text">Thyroid test is conducted to measure the function of the thyroid gland via a blood sample. The test measures values of:-<br> 
+                   • Thyroxine (T4), <br>
+                   • Triiodothyronine (T3) and<br> 
+                   • Thyroid stimulating hormone (TSH).<br>
                    </p>
                 )}
                 {this.state.nodeInnerText === "Liver" && (
-                  <p class="card-text">TLiver function test is used to ascertain the functioning of a person’s liver. The test measures parameters such as:- 
-                   • Total Protein, 
-                   • Albumin, 
-                   • Globulin, 
-                   • A/G Ratio, 
-                   • Total Bilirubin, 
-                   • Conjugated Bilirubin, 
-                   • Unconjugated Bilirubin, 
-                   • SGOT(AST), 
-                   • SGPT(ALT), 
-                   • Alkaline Phosphatase, 
-                   • GGT, 
-                   • SGOT/SGPT Ratio.
+                  <p class="card-text">TLiver function test is used to ascertain the functioning of a person’s liver. The test measures parameters such as:- <br>
+                   • Total Protein, <br>
+                   • Albumin, <br>
+                   • Globulin, <br>
+                   • A/G Ratio, <br>
+                   • Total Bilirubin,<br> 
+                   • Conjugated Bilirubin, <br>
+                   • Unconjugated Bilirubin, <br>
+                   • SGOT(AST), <br>
+                   • SGPT(ALT), <br>
+                   • Alkaline Phosphatase,<br> 
+                   • GGT, <br>
+                   • SGOT/SGPT Ratio.<br>
                    </p>
                 )}
                 {this.state.nodeInnerText === "Diabetes" && (
-                  <p class="card-text">HbA1c is a blood test that estimates the 90-day average blood glucose levels. HbA1c provides a longer-term trend on the blood sugar levels over a period of time. The test includes:- 
-                   • HbA1c%
-                   • Fetal Hemoglobin
-                   • Mean Plasma Glucose Level.
+                  <p class="card-text">HbA1c is a blood test that estimates the 90-day average blood glucose levels. HbA1c provides a longer-term trend on the blood sugar levels over a period of time. The test includes:- <br>
+                   • HbA1c% <br>
+                   • Fetal Hemoglobin <br>
+                   • Mean Plasma Glucose Level.<br>
                     </p>
                 )}
                 {this.state.nodeInnerText === "Vitamin D" && (
@@ -345,27 +345,27 @@ class Tests extends Component {
                   <p class="card-text">Vitamin B12 test is used to monitor the level of B12 in the body, it is analyzed via a blood sample. Vitamin B12 allows our DNA, red blood cells and nerves to carry out their normal, day to day activities.</p>
                 )}
                 {this.state.nodeInnerText === "Electrolytes & Minerals" && (
-                  <p class="card-text">An electrolyte panel is a blood test that measures the levels of electrolytes and carbon dioxide in your blood. Electrolytes measured are:-
-                   • Calcium
-                   • Sodium
-                   • Potassium
-                   • Chloride
+                  <p class="card-text">An electrolyte panel is a blood test that measures the levels of electrolytes and carbon dioxide in your blood. Electrolytes measured are:-<br>
+                   • Calcium<br>
+                   • Sodium<br>
+                   • Potassium<br>
+                   • Chloride<br>
                    </p>
                 )}
                 {this.state.nodeInnerText === "Anemia" && (
                   <p class="card-text">Anemia is usually caused by an iron deficiency in the blood. It is tested for using a blood sample. A low hemoglobin count is an indicator of anemia. Females usually have a lower hemoglobin level as compared to males.</p>
                 )}
                 {this.state.nodeInnerText === "Urine" && (
-                  <p class="card-text">Urine test may be used to identify UTIs, kidney, metabolic and other disorders. The test gives parameters such as:- 
-                   • RBCs Urine, 
-                   • Colour, 
-                   • Epithelial Cell, 
-                   • Urobilinogen, 
-                   • Leucocytes, 
-                   • Ketone, 
-                   • Protein Urine, 
-                   • Pus Cell and 
-                   • Crystals.
+                  <p class="card-text">Urine test may be used to identify UTIs, kidney, metabolic and other disorders. The test gives parameters such as:- <br>
+                   • RBCs Urine, <br>
+                   • Colour, <br>
+                   • Epithelial Cell, <br>
+                   • Urobilinogen, <br>
+                   • Leucocytes, <br>
+                   • Ketone, <br>
+                   • Protein Urine, <br>
+                   • Pus Cell and <br>
+                   • Crystals.<br>
                    </p>
                 )}
                 <button
